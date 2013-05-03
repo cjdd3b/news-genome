d2b564f133c7141f173bff4bf1630b5004a75638
Mark Grey <mark.grey@nytimes.com>
2013-05-03 13:39:24 -0400

    working article data source that excludes AP and Reuters articles

--- news_genome/__init__.py
+++ news_genome/__init__.py
@@ -3,6 +3,43 @@ import nltk
 import json
 from nltk.corpus import brown
 from HTMLParser import HTMLParser
+from datetime import datetime,timedelta
+import time
+import sys
+import collections
+import re
+
+start_date = datetime.utcnow()-timedelta(days=1)
+end_date = datetime.utcnow()
+
+class ArticleSource():
+    endpt = 'http://glass-output.nytimes.com/glass/outputmanager/v1/query.json?source=scoop&type=article&startdate=%i&enddate=%i' % (time.mktime(start_date.timetuple())*1000,time.mktime(end_date.timetuple())*1000)
+
+    def __init__(self):
+        try:
+            pages = json.loads(urllib.urlopen(self.endpt).read())
+            self.current = 0
+            regex = re.compile('nytimes.com/(aponline|external|reuters)')
+            self.queue = [key for key,val in pages.items() if not regex.search(key)]
+            print '%s articles loaded in queue' % len(self.queue)
+        except IOError:
+            print 'Connection Error'
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.current > len(self.queue):
+            raise StopIteration()
+
+        self.current+=1
+        url = 'http://glass-api.prd.use1.nytimes.com/glass/api/v1/lookup.json?collection=scoop_article&url=%s' % self.queue[self.current]
+        try:
+            article_data = json.loads(urllib.urlopen(url).read())
+            url,article = article_data.popitem()
+            return Article(article)
+        except IOError:
+            raise StopIteration()
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -18,11 +55,16 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
-data = json.loads(urllib.urlopen('http://glass-api.prd.use1.nytimes.com/glass/api/v1/lookup.json?collection=scoop_article&url=http://www.nytimes.com/2013/02/14/us/dorner-california-remains-police-shootout.html').read())
 
-key,article = data.popitem()
+class Article():
 
-summary = nltk.word_tokenize(article['data']['cms']['article']['summary'])
-body = nltk.word_tokenize(strip_tags(article['data']['cms']['article']['body']))
+    def __init__(self,data):
+        self.metadata = data
 
-print nltk.pos_tag(body)
+    def __str__(self):
+        return strip_tags(self.metadata['data']['cms']['article']['body'])
+
+
+if __name__ == '__main__':
+    articles = ArticleSource()
+    print articles.next()
